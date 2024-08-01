15af9150160b3928571a1724d1cdebaadd470815
Giri Yaram <yaramgiri@gmail.com>
2024-08-01 11:46:38 +0530

    changed title to prompt adviser

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -28,7 +28,7 @@ const Nav = () => {
           height={30}
           className='object-contain'
         />
-        <p className='logo_text_new'>Promptopia</p>
+        <p className='logo_text_new'>Prompt Adviser</p>
       </Link>
 
       {/* Desktop Navigation */}
